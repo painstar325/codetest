51a6444470aac34674d63639cb7e3afe0307d8f1
painstar <painstar325@gmail.com>
2023-04-15 15:50:11 +0900

    1270

---
+++ baekjoon/silver3/1270.py
@@ -0,0 +1,43 @@
+#https://www.acmicpc.net/problem/1270
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+# result = []
+results = []
+for i in range(N):
+    army = list(map(str, input().strip().split()))
+    team = army[0]
+    del army[0]
+    # army_set = set(army)
+    # flag = False
+    # result = ""
+    # army_len = len(army)
+    # for army_num in army_set:
+    #     army_count = army.count(army_num)
+    #     if army_count > (army_len / 2):
+    #         result = str(army_num)
+    #         break
+    #     result = "SYJKGW"
+    
+    army_len = len(army)
+    
+    army_dict = {}
+    for num in army:
+        if num not in army_dict:
+            army_dict[num] = 1
+        else:
+            army_dict[num] += 1
+    
+    for key, value in list(sorted(army_dict.items(), key=lambda item: item[1], reverse=True)):
+        if value + value > army_len:
+            results.append("SYJKGW")
+        else:
+            results.append(key)
+        break
+    
+    
+for result in results:
+    print(result)
